1ec4df190af0e4d0520661e65f16586a3e554e88
Seynshun <root@LAPTOP-78PI252I.localdomain>
2021-01-16 21:13:45 +0100

    transfer of my routes from the previous deposit

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -23,3 +23,65 @@ exports.getUser = (req, res) => {
     });
   });
 };
+exports.getNotifications = (req, res) => {
+  username = req.params.id;
+  Users.findOne({ username }).then(user => {
+    notification = user.notifications;
+    res.status(200).json({ notification })
+  });
+}
+
+exports.sendFriendRequest = (req, res) => {
+  username = req.params.id;
+  SenderUsername = req.session.username;
+  Users.findOne({ username }).then(user => {
+    user.notifications.friends.push({ SenderUsername });
+    user.save();
+    res.status(200).json({ message: "Friend request sent" })
+  })
+}
+
+exports.cancelFriendRequest = (req, res) => {
+  username = req.params.id;
+  SenderUsername = req.session.username;
+  Users.findOne({ username }).then(user => {
+    let updatedFriendNotifications = common.arrayRemove(user.notifications.friends, SenderUsername);
+    user.notifications.friends = updatedFriendNotifications;
+    user.save();
+    res.status(200).json({ message: "Friend request cancel" });
+  })
+}
+
+exports.acceptFriendRequest = (req, res) => {
+  friend = req.params.id;
+  username = req.session.username
+  Users.findOne({ username }).then(user => {
+    user.friendList.push(friend);
+    let updatedFriendNotifications = common.arrayRemove(user.notifications.friends, friend);
+    user.notifications.friends = updatedFriendNotifications;
+    user.save();
+    res.status(200).json({ message: "Friend added" });
+  })
+}
+
+exports.sendPlayingInvitation = (req, res) => {
+  username = req.params.id;
+  roomCode = req.body.roomCode
+  challengerUsername = req.session.username;
+  Users.findOne({ username }).then(user => {
+    user.notifications.games.push({ "username": challengerUsername, "roomCode": roomCode });
+    user.save();
+    res.status(200).json({ user })
+  });
+}
+
+exports.cancelPlayingInvitation = (req, res) => {
+  username = req.params.id;
+  challengerUsername = req.session.username;
+  Users.findOne({ username }).then(user => {
+    let remainingGames = common.arrayRemove(user.notifications.games, challengerUsername);
+    user.notifications.games = remainingGames;
+    user.save();
+    res.status(200).json({ remainingGames });
+  })
+}
