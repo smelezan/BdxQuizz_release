fc20fa6f799b42aec3664a1df7a59dae096a0d62
Wilfried <unconfigured@null.spigotmc.org>
2021-01-20 00:41:35 +0100

    update profile page

---
+++ client/bdxquizz-front/src/components/BarChart.vue
@@ -0,0 +1,15 @@
+<script>
+import { Bar } from "vue-chartjs";
+
+export default {
+    extends: Bar,
+    props:['chartdata'],
+    mounted() {
+        this.$refs.canvas.style.height = "300px";
+        this.renderChart(this.chartdata ,{ 
+            responsive: true, 
+            maintainAspectRatio: false
+        });
+    }
+};
+</script>
--- client/bdxquizz-front/src/views/Profile.vue
+++ client/bdxquizz-front/src/views/Profile.vue
@@ -19,14 +19,17 @@
                     </b-row>
                     <h4>Check out your stats below</h4>
                     <b-row class="w-75 d-flex align-items-center mx-auto my-5 shadow">
-                        <b-col>
+                        <b-col cols="6" md="5">
                             <strong>Quizz Played :</strong> {{nbQuizzPlayed}} <br />
                             <strong>Quizz Won :</strong> {{nbQuizzWon}} <br />
                             <strong>Quizz Lost :</strong> {{nbQuizzLost}} <br />
                             <strong>Quizz Ratio :</strong> {{nbQuizzWon/nbQuizzPlayed}}
                         </b-col>
-                        <b-col>
+                        <b-col cols="6" md="5" class="m-2">
                             <PieChart v-if="loaded" ref="quizz_chart" :chartdata="quizzData"></PieChart>
+                        </b-col>
+                        <b-col cols="12" md="12" class='m-2'>
+                            <BarChart v-if="loaded" ref="quizz_chart" :chartdata="catQuizzData"></BarChart>
                         </b-col>
                     </b-row>
                     <b-row class="w-75 d-flex align-items-center mx-auto my-5 p-2 shadow">
@@ -39,6 +42,9 @@
                         <b-col cols="12" md="12" class='m-2'>
                             <LineChart v-if="loaded" ref="score_chart" :chartdata="scoreData"></LineChart>
                         </b-col>
+                        <b-col cols="12" md="12" class='m-2'>
+                            <BarChart v-if="loaded" ref="quizz_chart" :chartdata="catScoreData"></BarChart>
+                        </b-col>
                     </b-row>
                     <b-row class="w-75 d-flex align-items-center mx-auto my-5 p-2 shadow">
                         <b-col cols="6" md="5" class='m-2'>
@@ -49,6 +55,9 @@
                         </b-col>
                         <b-col cols="12" md="12">
                             <LineChart v-if="loaded" ref="time_chart" :chartdata="timeData"></LineChart>
+                        </b-col>
+                        <b-col cols="12" md="12" class='m-2'>
+                            <BarChart v-if="loaded" ref="quizz_chart" :chartdata="catTimeData"></BarChart>
                         </b-col>
                     </b-row>
                 </b-container>
@@ -62,11 +71,13 @@
 import axios from 'axios';
 import PieChart from '@/components/PieChart';
 import LineChart from '@/components/LineChart';
+import BarChart from '@/components/BarChart';
 
     export default {
     components: {
         PieChart,
         LineChart,
+        BarChart
     },
     data() {
         return {
@@ -79,24 +90,33 @@ import LineChart from '@/components/LineChart';
             averageScore: 0,
             bestTime: 0,
             averageTime: 0,
+            categories: {},
+
             quizzData: null,
             scoreData: null,
             timeData: null,
+
+            catQuizzData: null,
+            catScoreData: null,
+            catTimeData: null,
         };
     },
     mounted() {
         axios.get('/api/stats/user').then((res) => {
-            console.log(res.data);
-                
             this.name = res.data.username;
 
             this.nbQuizzWon = res.data.stats.nbQuizzWon;
             this.nbQuizzLost = res.data.stats.nbQuizzLost;
             this.nbQuizzPlayed = res.data.stats.nbQuizzPlayed;
+
             this.bestScore = res.data.stats.bestScore;
             this.averageScore = res.data.stats.averageScore;
+
             this.bestTime = res.data.stats.bestTime;
             this.averageTime = res.data.stats.averageTime;
+
+            this.categories = res.data.stats.category;
+            console.log(this.categories);
 
             const scores = res.data.stats.scores;
             const times = res.data.stats.times;
@@ -128,8 +148,81 @@ import LineChart from '@/components/LineChart';
                     data: times
                 }]
             }
+            this.setCategoryChart();
             this.loaded = true;
         });
+    },
+    methods: {
+        setCategoryChart(){
+            let labels = [];
+
+            let dataWon = [];
+            let dataLost = [];
+            let dataPlay = [];
+
+            let dataBScore = [];
+            let dataAScore = [];
+
+            let dataBTime = [];
+            let dataATime = [];
+
+            for(const category in this.categories){
+                labels.push(category);
+                
+                dataWon.push(this.categories[category].nbQuizzWon);
+                dataLost.push(this.categories[category].nbQuizzLost);
+                dataPlay.push(this.categories[category].nbQuizzPlayed);
+
+                dataBScore.push(this.categories[category].bestScore);
+                dataAScore.push(this.categories[category].averageScore);
+
+                dataBTime.push(this.categories[category].bestTime);
+                dataATime.push(this.categories[category].averageTime);
+            }
+
+            this.catQuizzData = {
+                labels: labels,
+                datasets: [{
+                    label: "Quizz Play",
+                    backgroundColor: "#f1c40f",
+                    data: dataPlay
+                },{
+                    label: "Quizz Won",
+                    backgroundColor: "#00D8FF",
+                    data: dataWon
+                },{
+                    label: "Quizz Lost",
+                    backgroundColor: "#AA0055",
+                    data: dataLost
+                }]
+            }
+            
+            this.catScoreData = {
+                labels: labels,
+                datasets: [{
+                    label: "Best Score",
+                    backgroundColor: "#3eac3e",
+                    data: dataBScore
+                },{
+                    label: "Average Score",
+                    backgroundColor: "#8e44ad",
+                    data: dataAScore
+                }]
+            }
+
+            this.catTimeData = {
+                labels: labels,
+                datasets: [{
+                    label: "Best Time",
+                    backgroundColor: "#3eac3e",
+                    data: dataBTime
+                },{
+                    label: "Average Time",
+                    backgroundColor: "#8e44ad",
+                    data: dataATime
+                }]
+            }
+        }
     }
 };
 </script>
